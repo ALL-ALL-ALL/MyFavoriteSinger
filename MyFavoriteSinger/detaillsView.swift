51f5471ae5d44c9653f45547bc1e5cdcbe38479f
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-07-08 01:11:32 +0200

    en cours bientot

---
+++ MyFavoriteSinger/detaillsView.swift
@@ -0,0 +1,18 @@
+//
+//  detaillsView.swift
+//  MyFavoriteSinger
+//
+//  Created by  Ixart on 08/07/2024.
+//
+
+import SwiftUI
+
+struct detaillsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    detaillsView()
+}
