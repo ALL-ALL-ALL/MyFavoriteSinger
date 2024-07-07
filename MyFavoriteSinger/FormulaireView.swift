06bcd3ba9c542d97d57839061dfacb900cd34a04
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-07-07 04:45:32 +0200

    en cours

---
+++ MyFavoriteSinger/FormulaireView.swift
@@ -0,0 +1,18 @@
+//
+//  FormulaireView.swift
+//  MyFavoriteSinger
+//
+//  Created by  Ixart on 07/07/2024.
+//
+
+import SwiftUI
+
+struct FormulaireView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FormulaireView()
+}
