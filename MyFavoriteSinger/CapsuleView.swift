caffc8b8ea2bfee4653222b27855d653eb19940b
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-12-27 01:48:04 +0100

    en cours..capsule

---
+++ MyFavoriteSinger/CapsuleView.swift
@@ -0,0 +1,18 @@
+//
+//  CapsuleView.swift
+//  MyFavoriteSinger
+//
+//  Created by  Ixart on 27/12/2024.
+//
+
+import SwiftUI
+
+struct CapsuleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CapsuleView()
+}
