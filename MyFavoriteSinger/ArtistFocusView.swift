32f7061c1f858620beb0ac08dbb71e82f6247448
ALL-ALL-ALL <ixartallan5@gmail.com>
2025-01-02 01:15:38 +0100

    en cours .....

---
+++ MyFavoriteSinger/ArtistFocusView.swift
@@ -0,0 +1,18 @@
+//
+//  ArtistFocusView.swift
+//  MyFavoriteSinger
+//
+//  Created by  Ixart on 02/01/2025.
+//
+
+import SwiftUI
+
+struct ArtistFocusView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArtistFocusView()
+}
