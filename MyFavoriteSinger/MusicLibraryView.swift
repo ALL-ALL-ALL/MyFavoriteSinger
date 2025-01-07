b1e706061dd40c38ada4cc02850d10a17fa71283
ALL-ALL-ALL <ixartallan5@gmail.com>
2025-01-07 18:10:45 +0100

    en cours.. manque ux petite correction

---
+++ MyFavoriteSinger/MusicLibraryView.swift
@@ -0,0 +1,18 @@
+//
+//  MusicLibraryView.swift
+//  MyFavoriteSinger
+//
+//  Created by  Ixart on 07/01/2025.
+//
+
+import SwiftUI
+
+struct MusicLibraryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MusicLibraryView()
+}
---
+++ MyFavoriteSinger/MusicPlayerManager.swift
@@ -0,0 +1,8 @@
+//
+//  MusicPlayerManager.swift
+//  MyFavoriteSinger
+//
+//  Created by  Ixart on 07/01/2025.
+//
+
+import Foundation
